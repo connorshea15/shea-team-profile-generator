1ae7f56b10b0dfa778bbd49cff46e9a682f3104b
connorshea15 <connorshea15@gmail.com>
2020-07-30 16:12:51 -0700

    Passing tests left and right!

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -7,5 +7,10 @@ test('create a manager object', () => {
     expect(manager.id).toBe('43');
     expect(manager.email).toBe('mikesemail');
     expect(manager.officeNumber).toBe('43');
-    // comment
+});
+
+test('create a manager object', () => {
+    const manager = new Manager('Mike', '43', 'mikesemail', '43');
+
+    expect(manager.getName).toHaveProperty('name');
 });
