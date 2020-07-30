2d5eac9aec47ff32e58dabbc1099a5d46f8c38d8
connorshea15 <connorshea15@gmail.com>
2020-07-30 15:10:45 -0700

    Add test info

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,10 @@
+const Manager = require('../lib/Manager.js');
+
+test('create a manager object', () => {
+    const manager = new Manager('Mike', '43', 'mikesemail', '43');
+
+    expect(manager.name).toBe('Mike');
+    expect(manager.id).toBe('43');
+    expect(manager.email).toBe('mikesemail');
+    expect(manager.officeNumber).toBe('43');
+});
