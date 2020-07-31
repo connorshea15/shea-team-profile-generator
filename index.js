2ce6e2c9f62851dad8b2161e2f1f668cc197aece
connorshea15 <connorshea15@gmail.com>
2020-07-31 12:28:06 -0700

    Prompting the user for the proper inputs

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // This is where I will run my code
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+const inquirer = require('inquirer');
+
+
+
+// function to prompt the user
+const promptUser = function() {
+
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter the name of a member of your team!'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the email of this team member!'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Choose the role of this team member!',
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+    ]).then(employeeInfo => {
+        console.log(employeeInfo);
+    });
+};
+
+
+
+
+
+
+promptUser();
