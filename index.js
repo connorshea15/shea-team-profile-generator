fe85fb19fd80252650507600fd98c1e6299a4207
connorshea15 <connorshea15@gmail.com>
2020-08-01 13:59:12 -0700

    I am adding new employees to an array. I just need to figure out how to terminate the app

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
 const inquirer = require('inquirer');
 
+var employeeList = [];
+
 // I may need a global array that holds list of created employees 
 
 // that way my if (continue), else (call print template function)
@@ -34,48 +36,78 @@ const promptUser = function() {
             message: 'Choose the role of this team member!',
             choices: ['Manager', 'Engineer', 'Intern']
         }
-    ]).then(employeeInfo => {
-        if (employeeInfo.role === 'Manager') {
-            inquirer.prompt([
-                {
-                  type: 'input',
-                  name: 'officeNumber',
-                  message: 'Enter this managers office number!'
-                }]).then(officeNumber => {
-                    const member = new Manager(employeeInfo.name, '1', employeeInfo.email, officeNumber.officeNumber);
-                    console.log(member);
-                });
-        }  else if (employeeInfo.role === 'Engineer') {
-            inquirer.prompt([
-                {
-                  type: 'input',
-                  name: 'github',
-                  message: 'Enter this engineers github username!'
-                }]).then(github => {
-                    const member = new Engineer(employeeInfo.name, '1', employeeInfo.email, github.github);
-                    console.log(member);
-                });
-        } else if (employeeInfo.role === 'Intern') {
-            inquirer.prompt([
-                {
-                  type: 'input',
-                  name: 'school',
-                  message: 'Enter the school that this intern is attending!'
-                }]).then(school => {
-                    const member = new Intern(employeeInfo.name, '1', employeeInfo.email, school.school);
-                    console.log(member);
-                });
-        }
-        // This is where my conditional statement will go which will either
-        // call the prompt user function again or call the create html function
-        
-        }
-    });
+    ]);   
 };
 
+const promptRole = function(employee) {
+    if (employee.role === 'Manager') {
+        return inquirer.prompt([
+            {
+              type: 'input',
+              name: 'officeNumber',
+              message: 'Enter this managers office number!'
+            }]).then(officeNumber => {
+                const member = new Manager(employee.name, (employeeList.length + 1), employee.email, officeNumber.officeNumber);
+                // console.log(member);
+                // return member;
+                employeeList.push(member);
+                // console.log(employeeList);
+            });
+    } else if (employee.role === 'Engineer') {
+        return inquirer.prompt([
+            {
+              type: 'input',
+              name: 'github',
+              message: 'Enter this engineers github username!'
+            }]).then(github => {
+                const member = new Engineer(employee.name, (employeeList.length + 1), employee.email, employee.github);
+                // return member;
+                employeeList.push(member);
+                // console.log(employeeList);
+            });
+    } else if (employee.role === 'Intern') {
+        return inquirer.prompt([
+            {
+              type: 'input',
+              name: 'school',
+              message: 'Enter the school that this intern is attending!'
+            }]).then(school => {
+                const member = new Intern(employee.name, (employeeList.length + 1), employee.email, school.school);
+                // return member;
+                employeeList.push(member);
+                // console.log(employeeList);
+            });
+    }
+};
 
+const runAgain = function() {
+    inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'yesOrNo',
+          message: 'Would you like to add another empoyee?',
+          default: false
+        }]).then(yesOrNo => {
+            if (yesOrNo) {
+                console.log(employeeList);
+                return runApp();
+            } else {
+                console.log("populate the html page!");
+            }
+        });
+};
 
+// create empty array at top to hold list of employees
+// push new employee to the end of the array
+// prompt user to add more or stop
+const runApp = function() {
+    promptUser()
+        .then(employeeInfo => {
+            return promptRole(employeeInfo);
+        }).then(newEmployee => {
+            // return runAgain(newEmployee);
+            return runAgain();
+        });
+};
 
-
-
-promptUser();
+runApp();
