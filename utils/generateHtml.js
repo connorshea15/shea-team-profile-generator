fb9471c397c518bd6e83e62cf66fab91011bfe67
connorshea15 <connorshea15@gmail.com>
2020-08-01 16:04:58 -0700

    I am populating my employee string with the right info!

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,11 +1,31 @@
+const generateEmployeeSection = employeeList => {
+    // variable to hold html content for employees
+    let employeeString = "";
+    for (var i = 0; i < employeeList.length; i++) {
+        employeeString = employeeString.concat(
+            employeeList[i].getName() + "\n",
+            employeeList[i].getRole() + "\n",
+            employeeList[i].getId() + "\n",
+            employeeList[i].getEmail() + "\n",
+            employeeList[i].getOfficeNumber() + "\n",
+            "\n"
+        );
+    };
+
+    return employeeString;
+
+};
+
 const generateHTML = employees => {
-    console.log(
+    console.log(generateEmployeeSection(employees));
+    /* console.log(
+        generateEmployeeSection(employees);
         employees[0].getName() + "\n" +
         employees[0].getRole() + "\n" +
         employees[0].getId() + "\n" +
         employees[0].getEmail() + "\n" +
         employees[0].getOfficeNumber()
-    );
+    ); */
 }
 
 
